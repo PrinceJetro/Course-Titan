887dd9a1dcda9e7e37a3a1a24ead375b92af555b
PrinceJetro <adegbuyijephthah@gmail.com>
2024-11-06 14:32:24 +0100

    removed pyttsx3

--- main/views.py
+++ main/views.py
@@ -9,7 +9,6 @@ from django.http import HttpResponseForbidden
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.hashers import make_password
 from django.urls import reverse
-import pyttsx3
 from main.storage import SupabaseStorage
 
 
